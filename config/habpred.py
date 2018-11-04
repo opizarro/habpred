c8026230cbd1652a530d104bd0b942b8b4176ad6
Oscar <opizarro73@gmail.com>
2018-11-04 15:55:48 +1100

    Initial commit

---
+++ config/habpred.py
@@ -0,0 +1,21 @@
+# define the paths to the images directory
+IMAGES_PATH = ""
+
+# define the paths to the bathymetric patches
+BATHY_PATH = ""
+
+# define the path to the output training, validation and testing
+# HDF5 files
+TRAIN_HDF5 = ""
+VAL_HDF5 = ""
+TEST_HDF5 = ""
+
+# path to the output model file
+MODEL_PATH = "output/cgan_ae.model"
+
+# define the path to the dataset mean
+DATASET_MEAN = "output/habpred_image_mean.json"
+
+# define the path to the output directory used for storing plots,
+# classification reports, etc
+OUTPUT_PATH = "output"
